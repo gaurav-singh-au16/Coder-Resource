10b34628347a6ecf809d74f4c2f060509bf361d6
Gaurav Singh <gauravsingh.info@gmail.com>
2021-06-08 20:10:57 +0530

    database connected with app

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+require("dotenv").config()
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const app = express();
+const mongoose = require('mongoose')
+
+const {DATABASE} = process.env
+
+mongoose.connect(DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err) => {
+    if (err) throw err
+    console.log('DataBase Connected')
+})
 
 const homeRoutes = require("./routes/home")
 
@@ -15,6 +29,6 @@ app.use(express.static("script"))
 
 app.use("", homeRoutes)
 
-app.listen(process.env.PORT || 3000, () =>{
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server Started @ 3000")
 })
